1d649e050541df4f108da3abeeb6bfcde7964fa8
Daniel Arrais <danielarraiscarvalho@gmail.com>
2021-08-15 10:38:29 -0300

    Remove propriedade não mais utilizada

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,26 +3,6 @@ import RespositoryItem from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
-// 
-
-const repositories = [
-    {
-        name: "nlw-together-letmeask",
-        description: "NLW Edição 06",
-        url: "https://github.com/danielarrais/nlw-together-letmeask"
-    },
-    {
-        name: "react-main-concepts",
-        description: "Estudo dos conceitos básicos de React",
-        url: "https://github.com/danielarrais/react-main-concepts"
-    },
-    {
-        name: "clean-code-branas-io",
-        description: "Curso de Clean Code e Clean Arquitecture",
-        url: "https://github.com/danielarrais/clean-code-branas-io"
-    },
-]
-
 function RespositoryList() {
     const [repositories, setRepositories] = useState([]);
 
